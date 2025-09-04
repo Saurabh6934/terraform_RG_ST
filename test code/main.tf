0ff8d78bd3569b5908ae4041495dd0e265849074
Saurabh6934 <60789731+Saurabh6934@users.noreply.github.com>
2025-09-04 15:53:56 +0530

    Update main.tf

--- test code/main.tf
+++ test code/main.tf
@@ -1,5 +1,5 @@
 module "rgfolder" {
-  source = "C:\\Saurabh\\Terraform\\test_29aug\\Resource_group"
+  source = "./Resource_group"
   location = "West Europe"             # or any Azure region
   rgname   = "skp-rg29th"   # your desired resource group name
 
@@ -7,7 +7,7 @@ module "rgfolder" {
 
 module "strgdetails" {
   depends_on = [module.rgfolder]
-  source     = "C:\\Saurabh\\Terraform\\test_29aug\\Storage_acc"
+  source     = "./Storage_acc"
   storage_name = "ragsak6934"
   location = "West Europe"             # or any Azure region
   rgname   = "my-resource-group"   # your desired resource group name
